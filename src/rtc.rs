5d1dd51146f94ca2b96509f1255280084e52b647
Fundevoge <59122389+Fundevoge@users.noreply.github.com>
2024-09-06 12:34:17 +0200

    New version

--- src/rtc.rs
+++ src/rtc.rs
@@ -1,9 +1,8 @@
-use core::ptr::addr_of_mut;
-
-use chrono::{DateTime, NaiveDateTime, TimeZone as _};
+use chrono::{DateTime, TimeZone as _, Utc};
 use embassy_net::{tcp::TcpSocket, Ipv4Address};
 use embassy_sync::{blocking_mutex::raw::NoopRawMutex, signal::Signal};
-use embedded_svc::io::asynch::Read as _;
+use embassy_time::Timer;
+use embedded_svc::io::asynch::{Read as _, Write as _};
 use esp_hal::rtc_cntl::Rtc;
 use esp_wifi::wifi::{WifiDevice, WifiStaDevice};
 
@@ -11,7 +10,7 @@ use crate::tz_de::TzDe;
 
 #[derive(Copy, Clone, Debug)]
 pub(crate) struct RtcOffset {
-    secs: u64,
+    pub(crate) secs: u64,
 }
 
 fn get_rtc_offset(rtc: &Rtc, unix_time: u64) -> RtcOffset {
@@ -24,16 +23,16 @@ fn get_rtc_offset(rtc: &Rtc, unix_time: u64) -> RtcOffset {
 pub(crate) fn get_german_datetime(rtc: &Rtc, rtc_offset: &RtcOffset) -> DateTime<TzDe> {
     let micros = rtc.get_time_us();
     let unix_secs = micros / 1_000_000 + rtc_offset.secs;
-    let naive_dt =
-        NaiveDateTime::from_timestamp_opt(unix_secs as i64, ((micros % 1_000_000) * 1_000) as u32)
+    let dt_utc =
+        DateTime::<Utc>::from_timestamp(unix_secs as i64, ((micros % 1_000_000) * 1_000) as u32)
             .unwrap();
-    TzDe.from_utc_datetime(&naive_dt)
+    TzDe.from_utc_datetime(&dt_utc.naive_utc())
 }
 
 const RX_BUFFER_SIZE_TIME: usize = 1024;
-const TX_BUFFER_SIZE_TIME: usize = 64;
-static mut TIME_RX_BUFFER: [u8; RX_BUFFER_SIZE_TIME] = [0; RX_BUFFER_SIZE_TIME];
-static mut TIME_TX_BUFFER: [u8; TX_BUFFER_SIZE_TIME] = [0; TX_BUFFER_SIZE_TIME];
+const TX_BUFFER_SIZE_TIME: usize = 1024;
+// static mut TIME_RX_BUFFER: [u8; RX_BUFFER_SIZE_TIME] = [0; RX_BUFFER_SIZE_TIME];
+// static mut TIME_TX_BUFFER: [u8; TX_BUFFER_SIZE_TIME] = [0; TX_BUFFER_SIZE_TIME];
 
 #[embassy_executor::task]
 pub(crate) async fn rtc_adjust_task(
@@ -41,11 +40,11 @@ pub(crate) async fn rtc_adjust_task(
     rtc: &'static Rtc<'_>,
     rtc_offset_signal: &'static Signal<NoopRawMutex, RtcOffset>,
 ) {
-    let mut time_socket = TcpSocket::new(
-        wifi_program_stack,
-        unsafe { &mut *addr_of_mut!(TIME_RX_BUFFER) },
-        unsafe { &mut *addr_of_mut!(TIME_TX_BUFFER) },
-    );
+    let mut time_rx_buffer = [0; RX_BUFFER_SIZE_TIME];
+    let mut time_tx_buffer = [0; TX_BUFFER_SIZE_TIME];
+    let mut time_socket =
+        TcpSocket::new(wifi_program_stack, &mut time_rx_buffer, &mut time_tx_buffer);
+    // time_socket.set_timeout(Some(embassy_time::Duration::from_secs(10)));
     if let Err(time_connection_error) = time_socket
         .connect((Ipv4Address::new(192, 168, 178, 30), 3125))
         .await
