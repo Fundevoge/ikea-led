22d7a041612bc8c1919eeb1e5a2d415280c1e260
Fundevoge <59122389+Fundevoge@users.noreply.github.com>
2024-09-06 12:34:27 +0200

    Keepalive socket

--- src/network.rs
+++ src/network.rs
@@ -1,10 +1,11 @@
 use core::str::FromStr as _;
 
 use byteorder::ByteOrder as _;
+use embassy_executor::task;
 use embassy_net::{tcp::TcpSocket, Ipv4Address};
 use embassy_sync::{blocking_mutex::raw::NoopRawMutex, signal::Signal};
-use embassy_time::Duration;
-use embedded_svc::io::asynch::Read as _;
+use embassy_time::{Duration, Timer};
+use embedded_svc::io::asynch::{Read as _, Write};
 use esp_wifi::wifi::{WifiController, WifiDevice, WifiEvent, WifiStaDevice, WifiState};
 use micromath::F32Ext as _;
 
@@ -31,6 +32,7 @@ pub(crate) async fn connection(
             // wait until we're no longer connected
             controller.wait_for_event(WifiEvent::StaDisconnected).await;
             enabled_signal.reset();
+            log::warn!("[WIFI] DISCONNECTED");
             embassy_time::Timer::after(Duration::from_millis(5000)).await
         }
         if !matches!(controller.is_started(), Ok(true)) {
@@ -79,6 +81,40 @@ pub(crate) async fn start_stream(
         .await
         .unwrap();
     let fps = byteorder::LE::read_f32(&fps_decoder_buf);
-    log::info!("Playing at {fps:.2} FPS");
+    log::info!("Playing at {fps:.2}");
     *frame_duration_micros = (1_000_000.0 / fps).round() as u64;
+}
+
+#[task]
+pub(crate) async fn keep_alive(
+    wifi_program_stack: &'static embassy_net::Stack<WifiDevice<'static, WifiStaDevice>>,
+    keepalive_established_signal: &'static Signal<NoopRawMutex, ()>,
+) {
+    let mut keepalive_rx_buffer = [0; 64];
+    let mut keepalive_tx_buffer = [0; 1024];
+
+    let mut keepalive_socket = TcpSocket::new(
+        wifi_program_stack,
+        &mut keepalive_rx_buffer,
+        &mut keepalive_tx_buffer,
+    );
+
+    keepalive_socket.set_timeout(Some(Duration::from_secs(20)));
+
+    while let Err(e) = keepalive_socket
+        .connect((Ipv4Address::new(192, 168, 178, 30), 3126))
+        .await
+    {
+        log::warn!("Keepalive connection failed {e:?}, retrying...");
+        Timer::after_secs(10).await;
+    }
+    log::info!("Keepalive connected!");
+    keepalive_established_signal.signal(());
+
+    let keepalive_packet: [u8; 256] = [0b01010110; 256];
+
+    loop {
+        keepalive_socket.write_all(&keepalive_packet).await.unwrap();
+        Timer::after_secs(10).await;
+    }
 }
